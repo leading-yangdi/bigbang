ac421ce00693f1ccbb6e1ebe05c2eb68e98d27b4
leading-yangdi <1472288135@qq.com>
2020-11-02 11:15:30 +0800

    第二十四次提交

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -8,4 +8,15 @@ $.ajaxPrefilter(function (options) {
             Authorization: localStorage.getItem('token') || ''
         }
     }
+
+    // 全局统一拦截 complete 回调函数
+    // 不论成功还是失败,最终都会调用 comlete 回调函数
+    options.complete = function (res) {
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败!') {
+            // 1.强制清空 token
+            localStorage.removeItem('token')
+            // 2.强制跳转到登录页面
+            location.href = '/login.html'
+        }
+    }
 })

bc609de0ea561815c6aabd28642694f4bb81299d
leading-yangdi <1472288135@qq.com>
2020-11-02 21:45:07 +0800

    第四十八次提交

--- assets/js/article/art_list.js
+++ assets/js/article/art_list.js
@@ -1,6 +1,27 @@
 $(function () {
 
     var layer = layui.layer
+    var form = layui.form
+
+    // 定义美化事件的过滤器
+    template.defaults.imports.dataFormat = function (date) {
+        const dt = new Date(date)
+
+        var y = dt.getFullYear()
+        var m = padZero(dt.getMonth() + 1)
+        var d = padZero(dt.getDate())
+
+        var hh = padZero(dt.getHours())
+        var mm = padZero(dt.getMinutes())
+        var ss = padZero(dt.getSeconds())
+
+        return `${y}-${m}-${d} ${hh}:${mm}:${ss}`
+    }
+
+    // 定义补零的函数
+    function padZero(n) {
+        n > 9 ? n : '0' + n
+    }
 
     // 定义一个查询的参数对象
     var q = {
@@ -11,6 +32,7 @@ $(function () {
     }
 
     initTable()
+    initCate()
 
     // 获取文章列表数据的方法
     function initTable() {
@@ -28,4 +50,33 @@ $(function () {
             }
         })
     }
+
+    // 初始化分类的方法
+    function initCate() {
+        $.ajax({
+            method: 'GET',
+            url: '/my/article/cates',
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('获取分类数据失败!')
+                }
+                // 调用模板引擎渲染分类的可选项
+                var htmlStr = template('tpl-cate', res)
+                $('[name=cate_id]').html(htmlStr)
+                form.render()
+            }
+        })
+    }
+
+    // 为表单绑定事件
+    $('#form-search').on('submit', function (e) {
+        e.preventDefault()
+        // 获取表单中选中项
+        var cate_id = $('[name=cate_id]').val()
+        var state = $('[name=state]').val()
+        q.cate_id = cate_id
+        q.state = state
+
+        initTable()
+    })
 })

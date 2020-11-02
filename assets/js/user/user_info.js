0ea9ed896966f75ae7100e4110f7732641f76f47
leading-yangdi <1472288135@qq.com>
2020-11-02 14:24:54 +0800

    第二十八次提交

--- assets/js/user/user_info.js
+++ assets/js/user/user_info.js
@@ -22,6 +22,8 @@ $(function () {
                     return layer.msg('获取用户信息失败!')
                 }
                 console.log(res);
+                // 调用form.val()快速为表单赋值
+                form.val('formUserInfo', res.data)
             }
         })
     }

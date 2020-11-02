5a0dde3981cb6cb8045bdc7a2d813fd1c7905599
leading-yangdi <1472288135@qq.com>
2020-11-02 18:46:38 +0800

    第四十一次提交

--- assets/js/article/art_cate.js
+++ assets/js/article/art_cate.js
@@ -16,14 +16,32 @@ $(function () {
             }
         })
     }
-
+    var indexAdd = null
     // 为添加类别按钮绑定点击事件
     $('#btnAddCate').on('click', function () {
-        layer.open({
+        indexAdd = layer.open({
             type: 1,
             area: ['500px', '250px'],
             title: '添加文章文类',
             content: $('#dialog-add').html()
+        })
+    })
+
+    // 通过代理的形式,为form-add 表单绑定 submit 事件
+    $('body').on('submit', '#form-add', function (e) {
+        e.preventDefault()
+        $.ajax({
+            method: 'POST',
+            url: '/my/article/addcates',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('新增分类失败!')
+                }
+                initArtCateList()
+                layer.msg('新增分类成功!')
+                layer.close(indexAdd)
+            }
         })
     })
 })
